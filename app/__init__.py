dbcf4392ad55fb8b329c1592620625585647d287
Rodrigo <dalamangas@outlook.com>
2018-11-14 16:36:08 -0200

    __init__

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,5 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
-    from app import catalogo
+    from app import catalogo, client_secrets, crud, database
     return app
